a86301c601d6fbc05e518ad3c8325987784392ab
Seth Musulin <sethmusulin@gmail.com>
2014-05-07 16:41:08 -0600

    better variable names for trackerapi test
    Signed-off-by: Bebe Peng <bebepeng@gmail.com>

--- spec/models/tracker_api_spec.rb
+++ spec/models/tracker_api_spec.rb
@@ -7,30 +7,30 @@ describe TrackerApi do
   it 'returns an array of projects' do
     VCR.use_cassette('trackerapi/projects') do
       projects = @tracker.get_projects
-      projects.map! { |project| project['name'] }
+      project_names = projects.map { |project| project['name'] }
 
-      expect(projects).to include('Listen To')
-      expect(projects).to include("Bebe's User Authentication")
+      expect(project_names).to include('Listen To')
+      expect(project_names).to include("Bebe's User Authentication")
     end
   end
 
   it 'returns an array of stories' do
     VCR.use_cassette('trackerapi/stories') do
       stories = @tracker.get_stories('1075508')
-      stories.map! { |story| story["name"] }
+      story_names = stories.map { |story| story["name"] }
 
-      expect(stories).to include('As a user, I can view all of my projects')
-      expect(stories).to include('As a user, I can view all of the stories for a project')
+      expect(story_names).to include('As a user, I can view all of my projects')
+      expect(story_names).to include('As a user, I can view all of the stories for a project')
     end
   end
 
   it 'returns an array of all comments associated with a project' do
     VCR.use_cassette('trackerapi/comments') do
       comments = @tracker.get_comments('1075508')
-      comments.map! { |comment| comment["text"] }
+      comment_texts = comments.map { |comment| comment["text"] }
 
-      expect(comments).to include('This is a test comment on the first story')
-      expect(comments).to include('This is a second test comment on the first story')
+      expect(comment_texts).to include('This is a test comment on the first story')
+      expect(comment_texts).to include('This is a second test comment on the first story')
     end
   end
 end
